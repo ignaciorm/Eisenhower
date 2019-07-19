50d39fe351f196a8536b829f1cf78e9cf1a66d92
Ignacio Rodriguez <jirm2062@gmail.com>
2019-07-19 13:23:42 +0200

    grid of squares creted

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+let cols = rows = 9;
+
 function setup() {
   createCanvas(800,800);
   background(30);
-  
+
 }
 
 function draw(){
+  let thisy = height/10;
+  for (let i = 0; i < rows; i++) {
+    let thisx = width/10;
+    for (let i = 0; i < cols; i++) {
+      cuad(thisx,thisy);
+      thisx += height/10;
+    }
+    thisy += width/10;
+  }
 
+}
 
+function cuad(x,y){
+  rectMode(CENTER);
+  fill(140);
+  rect(x, y, 40, 40);
 }
